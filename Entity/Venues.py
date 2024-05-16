caf250cb0e39c2c91c1eb80362ae37c7fd9f0428
Taserwebs <abishekshanthy@gmail.com>
2024-05-16 21:28:13 +0530

    commit

---
+++ Entity/Venues.py
@@ -0,0 +1,9 @@
+class Venue:
+    def __init__(
+            self,
+            venue_name,
+            address,
+    ):
+        self.venue_name = venue_name
+        self.address = address
+        
