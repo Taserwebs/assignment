9bd631c68805fe60ce612f4374457471207afb90
Taserwebs <abishekshanthy@gmail.com>
2024-05-27 16:50:29 +0530

    commit

--- Interface/IEvent_service_provider.py
+++ Interface/IEvent_service_provider.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
 from Util.DBconnenction import DBConnection
-from Entity import Event
+from Entity import Events
 
 
 class IEvent_Service_Provider(ABC, DBConnection):
 
     @abstractmethod
-    def create_event(self, event: Event):
+    def create_event(self, event: Events):
         pass
 
     @abstractmethod
